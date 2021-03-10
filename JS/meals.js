09c7439909f34dd6c4b177ccabd03ef48ad55c4b
root <root@DESKTOP-BLO46UJ.localdomain>
2021-03-11 01:16:28 +0530

    added header and footer

--- JS/meals.js
+++ JS/meals.js
@@ -1,5 +1,6 @@
 var posts = document.getElementById('posts');
-var buttons = document.getElementById('buttons');
+var buttons1 = document.getElementById('buttons1');
+var buttons2 = document.getElementById('buttons2');
 const body = document.getElementById('posts')
 var limit = 8;
 var mockapi = [{
@@ -40,13 +41,28 @@ var mockapi = [{
             milk:true,
             drink:false,
             },
+            {
+                name:"Buon Appetito at table9",
+                back:"../Ganga/IMG/565-4023-l.jpg",
+                votes:15,
+                discount:-30,
+                hotel:"Table9 Cafe & Kitchen" ,
+                location: "Kuala Lumpur, Wilayah Persekutuan",
+                desc:"Indulge in a creative fare of Italian cuisine and Korean-inspired drinks in the cozy atmosphere of Table9.",
+                expiry:"12 Days",
+                egg:true,
+                milk:true,
+                drink:false,
+                },
         ]
 function createButton(){
     for(i=0;i<limit;i++){
         let button = document.createElement('button');
         button.setAttribute('id',i+1);
         button.innerHTML = i+1;
-        buttons.appendChild(button);
+        buttons1.appendChild(button);
+        buttons2.appendChild(button);
+      
     }
 
 }
@@ -68,11 +84,11 @@ function addPosts(){
 <button class="inline post-buttons">Get FREE Coupon</button> <div class="stars-outer"><div class="stars-inner"></div></div><p class="inline" style="margin-left: 2%; font-size: 12px;">(15)</p>
 <div class=" bordertop" style="margin-bottom: 0px;display: flex; flex-wrap: nowrap; padding: 0% 2%;margin-top: 10px;height: 55px;" >
      <div style="flex-direction: row;flex-grow: 1;width: 0%; padding-top: 10px;" class="borderright">
-         <span class="selected icons popup" onmouseover="myFunction()" onmouseout="mouseOut()"><img  class="scale" src="https://www.kindmeal.my/images/icon_egg.png" alt=""><span class="popuptext" id="myPopup">Contains egg</span></span><span class="selected icons popup" onmouseover="myFunction1()" onmouseout="mouseOut1()"><img class="scale" src="https://www.kindmeal.my/images/icon_milk.png" alt=""><span class="popuptext" id="myPopup1">Contains Dairy products<br/>(i.e milk,cheese)</span></span><span class="selected icons popup" onmouseover="myFunction2()" onmouseout="mouseOut2()"><img class="scale" src="https://www.kindmeal.my/images/icon_alcohol.png" alt=""><span class="popuptext" id="myPopup2">Contains Alcohol<br/>(i.e beer,wine,liqour)</span></span>
+         <span class="selected icons popup""><img  class="scale" src="https://www.kindmeal.my/images/icon_egg.png" alt=""><span class="popuptext" id="myPopup">Contains egg</span></span><span class="selected icons popup" onmouseover="myFunction1()" onmouseout="mouseOut1()"><img class="scale" src="https://www.kindmeal.my/images/icon_milk.png" alt=""><span class="popuptext" id="myPopup1">Contains Dairy products<br/>(i.e milk,cheese)</span></span><span class="selected icons popup" onmouseover="myFunction2()" onmouseout="mouseOut2()"><img class="scale" src="https://www.kindmeal.my/images/icon_alcohol.png" alt=""><span class="popuptext" id="myPopup2">Contains Alcohol<br/>(i.e beer,wine,liqour)</span></span>
      </div>
      <div style="flex-direction: row;flex-grow: 1; text-align:center; " class="borderright">
          <p style="margin: 3px 1px 1px 1px;">kindMEal Discount</p>
-         <h2 style="color: red;  margin:1px">${mockapi[i].discount}off</h2>
+         <h2 style="color: red;  margin:1px">${mockapi[i].discount} off</h2>
      </div>
      <div style="flex-direction: row;flex-grow: 1; text-align:center;  width: 0%;">
          <p style="margin: 1px;">Expires in</p>
@@ -82,31 +98,3 @@ function addPosts(){
  }
 }
 addPosts();
-
-var popup = document.getElementById("myPopup");
-var popup1 = document.getElementById("myPopup1");
-var popup2 = document.getElementById("myPopup2");
-// When the user clicks on div, open the popup
-function myFunction() {
-  popup.classList.toggle("show");
-}
-
-
-function mouseOut(){
-  popup.classList.toggle("show");
-}
-function myFunction1(){
-    popup1.classList.toggle("show");
-}
-
-function mouseOut1(){
-    popup1.classList.toggle("show");
-}
-
-function myFunction2(){
-    popup2.classList.toggle("show");
-}
-
-function mouseOut2(){
-    popup2.classList.toggle("show");
-}
