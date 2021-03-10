cb701f77b1c29d1ce223ec936f96255211d573f9
root <root@DESKTOP-BLO46UJ.localdomain>
2021-03-10 23:31:51 +0530

    added animations and posts template

--- JS/meals.js
+++ JS/meals.js
@@ -12,3 +12,31 @@ function createButton(){
 }
 
 createButton()
+
+var popup = document.getElementById("myPopup");
+var popup1 = document.getElementById("myPopup1");
+var popup2 = document.getElementById("myPopup2");
+// When the user clicks on div, open the popup
+function myFunction() {
+  popup.classList.toggle("show");
+}
+
+
+function mouseOut(){
+  popup.classList.toggle("show");
+}
+function myFunction1(){
+    popup1.classList.toggle("show");
+}
+
+function mouseOut1(){
+    popup1.classList.toggle("show");
+}
+
+function myFunction2(){
+    popup2.classList.toggle("show");
+}
+
+function mouseOut2(){
+    popup2.classList.toggle("show");
+}
